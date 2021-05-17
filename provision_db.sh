afb6d67927e8ccd8a2b597ac7648a5c846e56002
horothesun <horothesun@gmail.com>
2021-05-17 16:12:05 +0100

    'Provision Heroku Postgres DB' GitHub Action

--- provision_db.sh
+++ provision_db.sh
@@ -3,12 +3,19 @@
 [ -z "$HEROKU_API_KEY" ] && echo "Error: HEROKU_API_KEY must be defined" && exit 10
 [ -z "$HEROKU_APP_BASE_NAME" ] && echo "Error: HEROKU_APP_BASE_NAME must be defined" && exit 11
 
-export RANDOM_APP_NAME_SUFFIX=`LC_ALL=C tr -dc a-z0-9 < /dev/urandom | head -c 10 ; echo ''`
-export HEROKU_APP_NAME="$HEROKU_APP_BASE_NAME-$RANDOM_APP_NAME_SUFFIX"
+[ -z "$HEROKU_REGION" ] && HEROKU_REGION=eu
 
-heroku apps:create --no-remote --region eu --json $HEROKU_APP_NAME
+RANDOM_APP_NAME_SUFFIX=`LC_ALL=C tr -dc a-z0-9 < /dev/urandom | head -c 10 ; echo ''`
+HEROKU_APP_NAME=`echo $HEROKU_APP_BASE_NAME-$RANDOM_APP_NAME_SUFFIX | tr -s .,:\;_\ \\/ - | tr 'A-Z' 'a-z'`
+
+heroku apps:create --no-remote --region $HEROKU_REGION --json $HEROKU_APP_NAME
 
 heroku addons:create heroku-postgresql:hobby-dev --version=13 --app $HEROKU_APP_NAME --wait
 
-export HEROKU_POSTGRES_ADDON_NAME=`heroku addons --app $HEROKU_APP_NAME --json | jq -rM 'map(select(.addon_service.name == "heroku-postgresql")) | first | .name'`
+HEROKU_POSTGRES_ADDON_NAME=`heroku addons --app $HEROKU_APP_NAME --json | jq -rM 'map(select(.addon_service.name == "heroku-postgresql")) | first | .name'`
+
+echo ""
 echo "✅ '$HEROKU_APP_NAME' app created with '$HEROKU_POSTGRES_ADDON_NAME' Postgress add-on"
+echo ""
+echo "🔑 Get default DB credentials running the following:"
+echo "$ heroku pg:credentials:url --app $HEROKU_APP_NAME"
